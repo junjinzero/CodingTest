6974136f1484012f4250ddfb9125d860a8eeee14
jeonjin0 <130276995+junjinzero@users.noreply.github.com>
2024-03-19 15:00:07 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String msg) {
+        
+        char[] arr = msg.toCharArray();
+
+        int answer = arr.length * 2;
+
+        return answer;
+    }
+}
