c614ef99a0156702dffd25cf49df599131fa7b1b
jeonjin0 <130276995+junjinzero@users.noreply.github.com>
2024-11-01 11:18:22 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main
 {
@@ -9,32 +8,24 @@ public class Main
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] numArr = new int[28];
+        boolean[] isSubmitted = new boolean[30]; // 인덱스 0~29 사용, 초기값은 false
 
+        // 입력으로 받은 번호는 제출된 번호로 표시
         for (int i = 0; i < 28; i++)
         {
-            numArr[i] = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
+            isSubmitted[num - 1] = true;  // 인덱스 0부터 시작하므로 num-1
         }
 
-        Arrays.sort(numArr);
-
-        int t = 1;
-        for (int i = 0; i < numArr.length; i++)
+        // 제출되지 않은 번호 찾기
+        for (int i = 0; i < isSubmitted.length; i++)
         {
-            while(t != numArr[i])
+            if(!isSubmitted[i])
             {
-                System.out.println(t);
-                t++;
-                if(t == numArr[i]) break;
+                // false 이면 미제출자
+                // 번호로 출력하기 위해 +1
+                System.out.println(i + 1);
             }
-            t++;
-
-        }
-
-        while(t <= 30)
-        {
-            System.out.println(t);
-            t++;
         }
 
     }
